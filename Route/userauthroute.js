dd70c56370dc60715fdf707f16d21983ce200b62
developer111222 <61453529+developer111222@users.noreply.github.com>
2024-09-25 02:27:41 -0700

    Create userauthroute.js

---
+++ Route/userauthroute.js
@@ -0,0 +1,27 @@
+// src/routes/auth.js  
+const router = require('express').Router();  
+const passport = require('passport');  
+const jwt = require('jsonwebtoken');  
+const User = require('../Model/usermodel');  
+
+// Google Auth Route  
+router.get('/google', passport.authenticate('google', {  
+    scope: ['profile', 'email']  
+}));  
+
+// Google Auth Callback  
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), async (req, res) => {  
+
+    console.log(req.body)
+    const token = jwt.sign({ id: req.user.id }, 'kdhsfjfbndbfvnsdbfvsdnfbndb', { expiresIn: '1h' });  
+    res.cookie('token', token, { httpOnly: true });  
+    res.redirect('http://localhost:3000'); // Redirect to your frontend or wherever you want  
+});  
+
+// Extract user info  
+router.get('/me', (req, res) => {  
+    if (!req.user) return res.status(401).send({ message: 'Not authenticated' });  
+    res.json(req.user);  
+});  
+
+module.exports = router;
